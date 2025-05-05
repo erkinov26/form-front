ad1da7e960cbddaf4cb300c49623ee0dfa5bf12c
erkinov26 <erkinov0247@gmail.com>
2025-05-05 12:56:28 +0500

    lazy add

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import {
 } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '@/pages/home'
-import Registration from '@/pages/registration'
+import { lazy, Suspense } from 'react'
+const Registration = lazy(() => import('@/pages/registration'))
 
-// Create a client
 const queryClient = new QueryClient()
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route element={<Home />} path='/' />
-          <Route element={<Registration />} path='/registration' />
-        </Routes>
+        <Suspense fallback={<div></div>}>
+          <Routes>
+            <Route element={<Home />} path='/' />
+            <Route element={<Registration />} path='/registration' />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </QueryClientProvider>
   )
